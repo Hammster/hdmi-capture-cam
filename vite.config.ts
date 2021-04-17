9f6762f2e389950f55e559ce002e4398a79a822a
Hans Koch <mail@hans-koch.me>
2021-04-17 02:31:19 +0200

    Added check for https certificats

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,24 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import fs from 'fs'
 
+let server = {}
+
+// Add HTTPS if available for localhost testing
+if(fs.existsSync('localhost-key.pem') && fs.existsSync('localhost.pem')) {
+  server = {
+    open: true,
+    https: {
+      key: fs.readFileSync('localhost-key.pem'),
+      cert: fs.readFileSync('localhost.pem'),
+    },
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   define: {
     'process.env': process.env
   },
-  server: {
-    open: true,
-    https: {
-      key: fs.readFileSync('localhost-key.pem'),
-      cert: fs.readFileSync('localhost.pem'),
-    },
-  },
+  server
 })
